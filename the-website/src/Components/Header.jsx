e06760e74e16efcac0630d477aab65b3cde0fcb1
Alex <cupokoffi@gmail.com>
2022-02-14 11:16:19 -0500

    Added to Header

--- the-website/src/Components/Header.jsx
+++ the-website/src/Components/Header.jsx
@@ -8,7 +8,7 @@ function Header(){
             <Navbar /> 
             <div className='name'>
                 <h1 className='the-header'>CPE 322-A With Dr. Kevin Lu</h1>
-                <p className='details'>We'll see what happens to this website</p> 
+                <p className='details'>Group Members: Alex Gaskins, Nikola Ciric and Carlitos Rodriguez</p> 
                 <div className='header-btns'>
                     <Link to='features' className='active' smooth={true} duration={1000}><a className='cv-btn'>About</a></Link> 
                 </div>
