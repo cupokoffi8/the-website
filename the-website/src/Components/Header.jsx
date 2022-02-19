5ca6230f758c1672af0cfb6ccaf5f131c682fb55
Alex <cupokoffi@gmail.com>
2022-02-19 08:25:07 -0500

    Update group members

--- the-website/src/Components/Header.jsx
+++ the-website/src/Components/Header.jsx
@@ -8,7 +8,7 @@ function Header(){
             <Navbar /> 
             <div className='name'>
                 <h1 className='the-header'>CPE 322-A With Dr. Kevin Lu</h1>
-                <p className='details'>Group Members: Alex Gaskins, Nikola Ciric and Carlitos Rodriguez</p> 
+                <p className='details'>Group Members: Alex Gaskins, Nikola Ciric, Carlitos Rodriguez and Daniel Goldberg</p> 
                 <div className='header-btns'>
                     <Link to='features' className='active' smooth={true} duration={1000}><a className='cv-btn'>About</a></Link> 
                 </div>
