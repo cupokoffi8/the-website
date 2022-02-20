77696ccb0db101c9ada437b0ace4602789a71999
Jadyn Gomez <jadyn@Jadyns-MacBook-Air.local>
2022-02-20 18:21:31 -0500

    ll

--- the-website/src/Components/Header.jsx
+++ the-website/src/Components/Header.jsx
@@ -7,7 +7,7 @@ function Header(){
         <div id="main"> 
             <Navbar /> 
             <div className='name'>
-                <h1 className='the-header'>CPE 322-A With Dr. Kevin Lu</h1>
+                <h1 className='the-header'>CPE 322A With Dr. Kevin Lu</h1>
                 <p className='details'>Group Members: Alex Gaskins, Nikola Ciric, Carlitos Rodriguez and Daniel Goldberg</p> 
                 <div className='header-btns'>
                     <Link to='features' className='active' smooth={true} duration={1000}><a className='cv-btn'>About</a></Link> 
