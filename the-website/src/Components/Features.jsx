a3f5286c063205f4a4f1002d96b29f1868b3dee3
Alex <cupokoffi@gmail.com>
2022-02-28 13:21:06 -0500

    Image

--- the-website/src/Components/Features.jsx
+++ the-website/src/Components/Features.jsx
@@ -1,5 +1,6 @@
 import React from "react"; 
 import { Link } from 'react-scroll'; 
+import PP from "../Images/Pathosense.png"; 
 
 function Features(){
     return(
@@ -10,6 +11,7 @@ function Features(){
             <p>
             See what we have been working on
             </p>
+            <img id="pp" src={PP} />
             <div className="header-btns">
                 <a href="http://cupokoffi8.github.io/projects" className='cv-btn1'>Our Work</a> 
             </div> 
