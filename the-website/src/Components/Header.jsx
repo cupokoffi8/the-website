56a2b3aa284b90eb9f4e964395f70bfb8b8731ae
Alex <cupokoffi@gmail.com>
2022-03-03 20:06:03 -0500

    npm i aos@next

--- the-website/src/Components/Header.jsx
+++ the-website/src/Components/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react"; 
 import Navbar from './Navbar'; 
+import Aos from 'aos'; 
+import 'aos/dist/aos.css'; 
 import { Link } from 'react-scroll'; 
 
 function Header(){
+    Aos.init(); 
     return(
         <div id="main"> 
             <Navbar /> 
             <div className='name'>
-                <h1 className='the-header'>Welcome to Pathosense</h1>
-                <p className='details'>Our Team: Alex Gaskins, Nikola Ciric, Carlitos Rodriguez and Daniel Goldberg</p> 
-                <div className='header-btns'>
+                <h1 data-aos="zoom-in-down" data-aos-duration="2000" className='the-header'>Welcome to Pathosense</h1>
+                <p data-aos="fade-down" data-aos-duration="2400" className='details'>Our Team: Alex Gaskins, Nikola Ciric, Carlitos Rodriguez and Daniel Goldberg</p> 
+                <div data-aos="fade-up" data-aos-duration="2400" className='header-btns'>
                     <Link to='features' className='active' smooth={true} duration={1000}><a className='cv-btn'>About</a></Link> 
                 </div>
             </div>
